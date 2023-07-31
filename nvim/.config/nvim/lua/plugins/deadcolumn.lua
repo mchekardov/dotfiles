d700292268bae932ece69132da8530160dae649b
Mikhail <mchekardov@gmail.com>
2023-07-31 11:34:07 +0300

    feat(nvim): update config for deadcolumn.nvim

--- nvim/.config/nvim/lua/plugins/deadcolumn.lua
+++ nvim/.config/nvim/lua/plugins/deadcolumn.lua
@@ -1,4 +1,9 @@
 local opts = {
+  scope = "visible",
+  modes = function() return true end,
+  blending = {
+    threshold = 1, -- Disables the feature
+  },
   extra = {
     follow_tw = "+1",
   },
